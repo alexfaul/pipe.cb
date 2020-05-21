2dda2d3baf7a74c4dfc9241b82831ccff5101c0f
alexfaul <46567184+alexfaul@users.noreply.github.com>
2020-05-21 02:00:36 -0400

    options added in alignNidaq

--- functions/alignNidaq.m
+++ functions/alignNidaq.m
@@ -1,5 +1,8 @@
-function [dsnidaq,success]=alignNidaq(path)
+function [dsnidaq,success]=alignNidaq(path, graph)
 dataDirectory    ='Z:\AFdata\2p2019\';            %% Change if working in entirely different drive. If data falls anywhere below 2p2019, this is okay.
+%do I even need this?
+%% whether to generate graphs
+if nargin < 2, graph=1; end
 %% creating paths to load
 if contains(path,'.sbx')==1;            % Account for different potential inputs 
     root=extractBefore(path, '.sbx');   % So this can accept either paths without extension, with .sbx or _nidaq.mat at end
@@ -18,6 +21,10 @@ catch
     return
 end 
 %% binarize signals
+%%% THERE NEEDS TO BE A WAY TO MAKE THIS MORE ROBUST. We should always have
+%%% decent separation (>1.5), but if this changes, all signals binarized
+%%% wrong
+
 for i=1:length(nidaq.ensure);                             %%makes indexing much easier
     if nidaq.ensure(i)>1.5;
     nidaq2.ensure(i)=4;
@@ -74,6 +81,8 @@ end
 C=C+1;                                      % adjusts to be index of actual pulse instead of shifted by 1 bc of diff 
 C=C(1:nidaq.nframes);
 %% Downsampling by 2p pulses
+
+% CONDENSE THIS?
 dsnidaq.visstim=nidaq.visstim(C);
 dsnidaq.timeStamps2p=nidaq.timeStamps(C)';
 dsnidaq.shock=nidaq.shock(C);
@@ -107,6 +116,9 @@ for ii=2:(length(Ctemp))
    dsnidaq.licking(ii-1)=sum(lickOnsets(Ctemp(ii-1):Ctemp(ii)-1));          %extract for time period between frames, adjusted for window edges
 end                                                                         %sum across lickOnsets that occur w/in isolated window
 %% Plots to compare downsampled vs standard
+
+%clean this up
+if graph==1
 figure
 subplot(3,2,1)
 plot(dsnidaq.timeStamps2p,dsnidaq.shock)
@@ -144,7 +156,7 @@ plot(dsnidaq.timeStamps2p,dsnidaq.visstim);
 hold on
 plot(nidaq.timeStamps, nidaq.visstim)
 ylim([-1 6]), title('Visstim')
-
+end
 %% Save structure
 [filepath]=fileparts(nidaqPath)
 Filename=[filepath,'\',nidaq.mouse,'_',num2str(nidaq.date),'_', num2str(nidaq.run),'_','dsNidaq'];
