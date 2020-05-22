a63abfcb21a81c214b1115db2e1a32444f1a07f5
alexfaul <46567184+alexfaul@users.noreply.github.com>
2020-05-22 03:03:50 -0400

    Updated wrapper style

--- cleanWrapper1.m
+++ cleanWrapper1.m
@@ -4,11 +4,10 @@
 % files to run in second line of indexing after findFilePathAF
 %% Find Files of Interest
 %root='E:\2Photon\Data'
-root    ='Z:\DATA_TEMP\New folder';            %% as character 
-ext ='.sbx';                                   %% Can find any character (text) matching ext. Also faster than the File Explorer
-sbxdirs = findFILE(root,ext);                  %% Will return cell array of all files under root containing extList
-
-sbxDirs = sbxdirs(1);                      %% Sub-select files you want to continue in pipeline with by indexing 
+    root    ='Z:\DATA_TEMP\New folder';            %% as character 
+    ext ='.sbx';                                   %% Can find any character (text) matching ext. Also faster than the File Explorer
+    sbxdirs = findFILE(root,ext);                  %% Will return cell array of all files under root containing extList
+    sbxDirs = sbxdirs(1);                      %% Sub-select files you want to continue in pipeline with by indexing 
 %% Write TIFFs - OPTIONAL! CAN RUN .Sbx DIRECTLY IN SUITE2P on YB-2
 for ii=1:length(sbxDirs);
 tiffLoop(sbxDirs{ii},1000,0);   %% write TIFFs for all sbx files found above to run suite2p
@@ -25,10 +24,10 @@ for ii=1:length(sbxDirs); %will output a _dsnidaq.mat file
 [dsnidaq,success]=alignNidaq(sbxDirs{ii}); 
 end
 %% Eye vids - find and write them to .avi
-root   = 'Z:\AFdata\2p2019\W05';
-ext    = 'eye.mat';
-eyedirs= findFILE(root,ext);
-eyeDirs= eyedirs(16:end);
+    root   = 'Z:\AFdata\2p2019\W05';
+    ext    = 'eye.mat';
+    eyedirs= findFILE(root,ext);
+    eyeDirs= eyedirs(16:end);
 
 for ii=1:length(eyeDirs)
 [temp]=writeVidAF(eyeDirs{ii}); % change this so that it will find the eye.mat files from the sbx directories
--- cleanWrapper2.m
+++ cleanWrapper2.m
@@ -6,29 +6,29 @@
 % If using FM, convert outputs with jupyter notebook 
 %   (Open anaconda prompt, type jupyter notebook, adjust filepath in facemap processing, run - comment out pupil if only doing motSVD)
 %% Get BHV paths to find stimuli/response onsets/offsets
-root='Z:\DATA_TEMP\New folder';
-ext = 'bhv2';
-bhvdirs = findFILE(root,ext);
-bhvPath = bhvdirs(1);
-%% Path to file with conditions listed
+    root='Z:\DATA_TEMP\New folder';
+    ext = 'bhv2';
+    bhvdirs = findFILE(root,ext);
+    bhvPath = bhvdirs(1);
 
 for ii=1:length(bhvPath);   
-    [~]= bhv2Convert(bhvPath{ii});                               %% MAKE SURE THERE IS A RUN # in THE .bhv2 FILE NAME (A 001 or 002 etc) IN THERE
-end                                                                     % config file (.txt with MonkeyLogic Conditions) should be in the 2p data folder
+   [~]= bhv2Convert(bhvPath{ii});                               %% MAKE SURE THERE IS A RUN # in THE .bhv2 FILE NAME (A 001 or 002 etc) IN THERE
+end                                                              % config file (.txt with MonkeyLogic Conditions) should be in the 2p data folder
 % configfile=fullfile(which([cname,'.txt']));
 %% find stim times from nidaq output pulses
-ext = 'dsNidaq';
-nidaqdirs = findFILE(root,ext);
-nidaqDirs=nidaqdirs(:);
+    ext = 'dsNidaq';
+    nidaqdirs = findFILE(root,ext);
+    nidaqDirs=nidaqdirs(:);
+
 for ii=1:length(nidaqDirs);
-Stim=stimTimes(nidaqDirs{ii});
+   Stim=stimTimes(nidaqDirs{ii});
 end 
-%% make generate cell plots and generate behavior plots 2 separate functions
- %Run
-ext = '_stim';
-stimdirs = findFILE(root,ext);
-stimDirs=stimdirs(3);
-time_window=90;
+%% plots of behavior (pupil, running, etc) during trials w/ stim
+    ext = '_stim';
+    stimdirs = findFILE(root,ext);
+    stimDirs=stimdirs(3);
+    time_window=90;
+
 for ii=1:length(stimDirs)
-behaviorPlots2(stimDirs{ii},time_window,['FC Day 3 - Expression']) %add XY offset
+   behaviorPlots2(stimDirs{ii},time_window,'FC Day 3 - Expression')           % last is custom label (useful for labeling behavioral day ex: FC) 
 end 
