4d4a196c4674f30d2fcdb3e7b3a177d363b68f97
alexfaul <46567184+alexfaul@users.noreply.github.com>
2020-05-28 22:57:05 -0400

    example pipeline status of data using unique extensions

---
+++ pipelineTrackingExample.m
@@ -0,0 +1,21 @@
+%% Example code using findFILE to isolate files that need to be run through pipeline:
+
+    root    ='Z:\AFdata\2p2019\Sut1';                    %% as character 
+    ext = '.avi';
+    aviDir = findFILE(root,ext);
+
+    ext = 'eye.mat';
+    eyeDir = findFILE(root,ext);
+        
+fileBaseAVI=extractBefore(aviDir,'.avi')
+fileBaseMAT=extractBefore(eyeDir,'.mat')
+
+needsAvi =setdiff(fileBaseMAT,fileBaseAVI) 
+%% for finding things run through suite2p and cell clicked
+    root    ='Z:\AFdata\2p2019\Sut2';                    %% as character 
+    ext = 'stat.npy';
+    suite2pDir = findFILE(root,ext);
+
+     ext = 'Fall.mat';
+    clickedDir = findFILE(root,ext);
+    
